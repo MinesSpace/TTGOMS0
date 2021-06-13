3b3f18891dcd34d9aadb1f32b6532f97cf551044
Miailhe Paul <paul.miailhe@gmail.com>
2021-06-13 03:35:02 +0200

    add lora system

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
 #include <MicroNMEA.h>
 
 #include "TTGOMS0.h"
+#include <RadioLib.h>
 
 SFE_UBLOX_GPS myGPS;
+SX1276 radio = new Module(RADIO_CS_PIN, RADIO_DI0_PIN, RADIO_RST_PIN, RADIO_BUSY_PIN);
 
 char nmeaBuffer[100];
 MicroNMEA nmea(nmeaBuffer, sizeof(nmeaBuffer));
@@ -17,45 +19,43 @@ void setup() {
     // When the power is turned on, a delay is required.
     delay(1500);
 
-    Serial.println("SparkFun Ublox Example");
-
+    
     if (myGPS.begin(Serial1) == false) {
         Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
         while (1);
     }
+
+    Serial.print(F("[SX1276] Initializing ... "));
+    int state = radio.begin(868.0, 500, 6, 7, SX127X_SYNC_WORD, 10, 8, 0);
+    state = radio.setCurrentLimit(60);
+    state = radio.setCRC(true);
+
+    if (state == ERR_NONE) {
+        Serial.println(F("success!"));
+    } 
+    else {
+        Serial.print(F("failed, code "));
+        Serial.println(state);
+        while (true);
+    }
+    
   
 }
 
 void loop() {
       myGPS.checkUblox(); //See if new data is available. Process bytes as they come in.
 
+        long altitulde_mm = 0;
+        long latitude_mdeg = 0;
+        long longitude_mdeg = 0;
+
     if (nmea.isValid() == true) {
-        long latitude_mdeg = nmea.getLatitude();
-        long longitude_mdeg = nmea.getLongitude();
+        latitude_mdeg = nmea.getLatitude();
+        longitude_mdeg = nmea.getLongitude();
         
-        long altitulde_mm;
+        altitulde_mm;
         nmea.getAltitude(altitulde_mm);
-
-        byte latitudeByte[4];
-        byte longitudeByte[4];
-        byte altituldeByte[4];
-
-        latitudeByte[0] = (int)((latitude_mdeg >> 24) & 0xFF);
-        latitudeByte[1] = (int)((latitude_mdeg >> 16) & 0xFF) ;
-        latitudeByte[2] = (int)((latitude_mdeg>> 8) & 0XFF);
-        latitudeByte[3] = (int)((latitude_mdeg & 0XFF));
-
-        longitudeByte[0] = (int)((longitude_mdeg >> 24) & 0xFF);
-        longitudeByte[1] = (int)((longitude_mdeg >> 16) & 0xFF) ;
-        longitudeByte[2] = (int)((longitude_mdeg>> 8) & 0XFF);
-        longitudeByte[3] = (int)((longitude_mdeg & 0XFF));
-
-        altituldeByte[0] = (int)((altitulde_mm >> 24) & 0xFF);
-        altituldeByte[1] = (int)((altitulde_mm>> 16) & 0xFF) ;
-        altituldeByte[2] = (int)((altitulde_mm>> 8) & 0XFF);
-        altituldeByte[3] = (int)((altitulde_mm & 0XFF));
-
-        
+    
 
         Serial.print("Latitude (deg): ");
         Serial.println(latitude_mdeg / 1000000., 6);
@@ -71,7 +71,57 @@ void loop() {
         Serial.println(nmea.getNumSatellites());
     }
 
+    Serial.print(F("[SX1276] Transmitting packet ... "));
+
+    byte byteArr[14];
+
+    byteArr[0] = (int)((latitude_mdeg >> 24) & 0xFF);
+    byteArr[1] = (int)((latitude_mdeg >> 16) & 0xFF) ;
+    byteArr[2] = (int)((latitude_mdeg>> 8) & 0XFF);
+    byteArr[3] = (int)((latitude_mdeg & 0XFF));
+
+    byteArr[4] = (int)((longitude_mdeg >> 24) & 0xFF);
+    byteArr[5] = (int)((longitude_mdeg >> 16) & 0xFF) ;
+    byteArr[6] = (int)((longitude_mdeg>> 8) & 0XFF);
+    byteArr[7] = (int)((longitude_mdeg & 0XFF));
+
+    byteArr[8] = (int)((altitulde_mm >> 24) & 0xFF);
+    byteArr[9] = (int)((altitulde_mm>> 16) & 0xFF) ;
+    byteArr[10] = (int)((altitulde_mm>> 8) & 0XFF);
+    byteArr[11] = (int)((altitulde_mm & 0XFF));
+
+    
+    int state = radio.transmit(byteArr, 12);
+
     delay(250); //Don't pound too hard on the I2C bus
+
+    if (state == ERR_NONE) {
+        // the packet was successfully transmitted
+        Serial.println(F(" success!"));
+
+        // print measured data rate
+        Serial.print(F("[SX1276] Datarate:\t"));
+        Serial.print(radio.getDataRate());
+        Serial.println(F(" bps"));
+    }
+
+    else if (state == ERR_PACKET_TOO_LONG) {
+        // the supplied packet was longer than 256 bytes
+        Serial.println(F("too long!"));
+
+    } else if (state == ERR_TX_TIMEOUT) {
+        // timeout occurred while transmitting packet
+        Serial.println(F("timeout!"));
+
+    } else {
+        // some other error occurred
+        Serial.print(F("failed, code "));
+        Serial.println(state);
+
+    }
+
+    // wait for a second before transmitting again
+    delay(1000);
   
 }
 
